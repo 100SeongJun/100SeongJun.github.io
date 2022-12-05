d9f7989edc3543e3feb726bdabf89f63fd1e1269
100Seongjun <qortjdwns120712@gmail.com>
2022-12-06 02:32:11 +0900

    chore : Split components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
-import "./styles.css";
-import { Body } from "./container/Body";
-import { Header } from "./container/Header";
+import { useEffect, useState } from 'react';
+import './styles.css';
+import { Body, ScrollTop, Footer, Header } from './container';
 function App() {
   const [theme, setTheme] = useState(false);
   const handleTheme = () => {
@@ -10,57 +9,34 @@ function App() {
 
   useEffect(() => {
     const scrollhead = () => {
-      const headroom = document.getElementById("");
-    };
-  });
-  
-  useEffect(() => {
-    const scrollUp = () => {
-      const btnScrollTop =
-        document.querySelector(".scroll-top");
-      if (
-        document.getElementById("top") > 500 ||
-        document.documentElement.scrollTop > 500
-      ) {
-        btnScrollTop.style.display = "block";
-      } else {
-        btnScrollTop.style.display = "none";
-      }
-    };
-    document.addEventListener("scroll", scrollUp);
-    return () => {
-      document.removeEventListener("scroll", scrollUp);
+      const headroom = document.getElementById('');
     };
   }, []);
+
+  useEffect(() => {
+    const scrollUp = () => {
+      const btnScrollTop = document.querySelector('.scroll-top');
+      if (
+        document.getElementById('top') > 500 ||
+        document.documentElement.scrollTop > 500
+      ) {
+        btnScrollTop.style.display = 'block';
+      } else {
+        btnScrollTop.style.display = 'none';
+      }
+    };
+    document.addEventListener('scroll', scrollUp);
+    return () => {
+      document.removeEventListener('scroll', scrollUp);
+    };
+  }, []);
+
   return (
-    
-    <div
-      className={theme ? "contain dark" : "contain light"}
-      id="top"
-    >
-      <Header handleTheme={handleTheme} theme={theme}/>
-
+    <div className={theme ? 'contain dark' : 'contain light'} id="top">
+      <Header handleTheme={handleTheme} theme={theme} />
       <Body />
-
-      <footer className="footer">
-        <a
-          href="https://github.com/rajshekhar26/cleanfolio-minimal"
-          className="link footer__link"
-        >
-          Created By Raj Shekhar
-        </a>
-      </footer>
-
-      <div className="scroll-container">
-        <div className="scroll-top">
-          <a aria-label="Scroll up" href="#top">
-            <i
-              aria-hidden="true"
-              className="fas fa-arrow-up"
-            ></i>
-          </a>
-        </div>
-      </div>
+      <Footer />
+      <ScrollTop />
     </div>
   );
 }
