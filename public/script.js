a4fa00f3d5e3ec331f99418030cee6474d3ff175
100Seongjun <qortjdwns120712@gmail.com>
2022-12-06 00:01:19 +0900

    addEventListener

--- public/script.js
+++ public/script.js
@@ -1,17 +1,41 @@
-setTimeout(()=>{
-const btnHamburger = document.getElementsByClassName("fa-bars")[0];
+setTimeout(() => {
+  //Scroll TopBtn
+  const btnHamburger = document.getElementsByClassName('fa-bars')[0];
 
-const displayList = () => {
-  const navUl = document.querySelector(".nav__list");
-  if (btnHamburger.classList.contains("fa-bars")) {
-    btnHamburger.classList.remove("fa-bars");
-    btnHamburger.classList.add("fa-times");
-    navUl.classList.add("display-nav-list");
-  } else {
-    btnHamburger.classList.remove("fa-times");
-    btnHamburger.classList.add("fa-bars");
-    navUl.classList.remove("display-nav-list");
-  }
-};
-btnHamburger.addEventListener("click", displayList);
-},300)
+  const displayList = () => {
+    const navUl = document.querySelector('.nav__list');
+    if (btnHamburger.classList.contains('fa-bars')) {
+      btnHamburger.classList.remove('fa-bars');
+      btnHamburger.classList.add('fa-times');
+      navUl.classList.add('display-nav-list');
+    } else {
+      btnHamburger.classList.remove('fa-times');
+      btnHamburger.classList.add('fa-bars');
+      navUl.classList.remove('display-nav-list');
+    }
+  };
+  btnHamburger.addEventListener('click', displayList);
+
+  // ScrollHeader
+  const header = document.getElementsByClassName('header')[0];
+  const headerScroll = (direction) => {
+    if (direction === 'up') {
+      header.classList.remove('scrollDown');
+    } else if (direction === 'down') {
+      header.classList.add('scrollDown');
+    }
+  };
+  var prevScrollTop = 0;
+
+  const headerScrollFnc = () => {
+    var nextScrollTop = window.scrollY || 0;
+    if (nextScrollTop > prevScrollTop) {
+      headerScroll('down');
+    } else if (nextScrollTop < prevScrollTop) {
+      headerScroll('up');
+    }
+    prevScrollTop = nextScrollTop;
+  };
+
+  document.addEventListener('scroll', () => headerScrollFnc());
+}, 100);
