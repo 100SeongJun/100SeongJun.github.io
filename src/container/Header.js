0d10c2b7abca6ed0bc61575fef9871dc7eefd2f2
SJ <100SeongJun>
2023-01-07 07:04:42 +0900

    FEAT : Add ChangeTheme

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,52 +1,56 @@
-// import { useState } from "react"
+import { Outlet } from "react-router-dom";
+import "../styles.css";
+export const Header = ({ changeTheme }) => {
+  const navbar = ["projects", "skills", "contact"];
 
-export const Header = ({ handleTheme, theme }) => {
+  const handleTheme = () => {
+    let theme = window.localStorage.getItem("theme");
+    if (theme === "light") {
+      window.localStorage.setItem("theme", "dark");
+    } else if (theme === "dark") {
+      window.localStorage.setItem("theme", "light");
+    }
+    changeTheme();
+  };
   return (
-    <div className="header center">
-      <h3>
-        <a
-          href="https://rajshekhar26.github.io/cleanfolio-minimal"
-          className="link"
-        >
-          100SeongJun's Portfolio
-        </a>
-      </h3>
+    <>
+      <div className="header center">
+        <h3>
+          <a href="/" className="link">
+            100SeongJun's Portfolio
+          </a>
+        </h3>
+        <nav className="nav center">
+          <ul className="nav__list center">
+            {navbar.map((nav, idx) => {
+              return (
+                <li className="nav__list-item" key={idx}>
+                  <a className="link link--nav" href={`/#${nav}`}>
+                    {nav}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
 
-      <nav className="nav center">
-        <ul className="nav__list center">
-          <li className="nav__list-item">
-            <a className="link link--nav" href="#projects">
-              Projects
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a className="link link--nav" href="#skills">
-              Skills
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a className="link link--nav" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
+          <button onClick={handleTheme}>
+            {window.localStorage.getItem("theme") === "dark" ? (
+              <i className="fas fa-sun" />
+            ) : (
+              <i className="fas fa-moon"></i>
+            )}
+          </button>
 
-        <button onClick={handleTheme}>
-          {theme ? (
-            <i className="fas fa-sun" />
-          ) : (
-            <i className="fas fa-moon"></i>
-          )}
-        </button>
-
-        <button
-          type="button"
-          aria-label="toggle navigation"
-          className="btn btn--icon nav__hamburger"
-        >
-          <i aria-hidden="true" className="fas fa-bars" />
-        </button>
-      </nav>
-    </div>
+          <button
+            type="button"
+            aria-label="toggle navigation"
+            className="btn btn--icon nav__hamburger"
+          >
+            <i aria-hidden="true" className="fas fa-bars" />
+          </button>
+        </nav>
+      </div>
+      <Outlet />
+    </>
   );
 };
