45d3e81e1bdf8973c02886db0df6858dbfefaedd
SJ <100SeongJun>
2023-01-07 06:56:25 +0900

    REMOVE : githubAPI

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-export const githubApi = (setState) => {
-  client({
-    url: `users/100Seongjun/repos`,
-    method: 'get',
-  }).then((res) => setState(res.data));
-};
