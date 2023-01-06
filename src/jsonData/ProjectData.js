9c3029f84db134953cf93d365e7e0ec0563e7069
SJ <100SeongJun>
2023-01-07 07:14:32 +0900

    FEAT : Add ProjectData

---
+++ src/jsonData/ProjectData.js
@@ -0,0 +1,41 @@
+const ProjectData = [
+  {
+    Title: "ERP 시스템",
+    Subject: "근태 및 일정 관리 프로젝트",
+    Contents: "사내 팀의 근태와 일정을 관리하는 그룹웨어 서비스",
+    Skills: [
+      "Java",
+      "SpringBoot",
+      "JPA",
+      "React",
+      "JS",
+      "AWS",
+      "Travis",
+      "Docker",
+      "Redis",
+    ],
+    URI: "",
+    Image: "",
+    card: ["하나", "둘", "셋", "넷"],
+  },
+  {
+    Title: "Pixabay Clone Coding",
+    Subject: "Pixabay 참고 클론코딩",
+    Contents: "내용",
+    Skills: ["React", "JS", "CSS", "HTML"],
+    URI: "",
+    Image: "/assets/pixabayImage.png",
+    card: ["111", "222", "33", "44"],
+  },
+  {
+    Title: "Pixabay Clone Coding",
+    Subject: "Pixabay 참고 클론코딩",
+    Contents: "내용",
+    Skills: ["React", "JS", "CSS", "HTML"],
+    URI: "",
+    Image: "/assets/pixabayImage.png",
+    card: ["1a11", "a33", "4a4"],
+  },
+];
+
+export default ProjectData;
